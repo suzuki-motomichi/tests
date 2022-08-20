5a3b3589149cf42423996f86527a51a63f484cbf
suzuki-motomichi <cw.sm31@gmail.com>
2022-08-20 19:56:41 +0900

    テンプレートの作成

---
+++ frontend/app/src/components/organisms/LoginForm.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Input from "../atoms/Input";
+import PasswordInput from "../atoms/PasswordInput";
+import PrimaryButton from "../atoms/PrimaryButton";
+import { Box } from "@mui/system";
+import { Link } from "react-router-dom";
+import Or from "../atoms/Or";
+import { CardContent } from "@mui/material";
+import BasicCard from "../atoms/BasicCard";
+import { theme } from "../../style/theme";
+
+type State = {
+  mail: string;
+  password: string;
+  isAgreement: boolean;
+};
+
+const LoginForm: React.FC = () => {
+  const [values, setValues] = React.useState<State>({
+    mail: "",
+    password: "",
+    isAgreement: false,
+  });
+
+  const handleStateInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleClickButton = () => {
+    console.log({ values });
+  };
+
+  return (
+    <>
+      <Box sx={{ textAlign: "center", m: 0 }}>
+        <Box sx={{ mt: 8, mb: 5 }}>
+          <h2>ログイン</h2>
+        </Box>
+        <BasicCard>
+          <CardContent sx={cardContentStyle}>
+            <Input
+              name={"mail"}
+              label={"メールアドレス"}
+              handleChangeText={handleStateInputChange}
+            />
+            <PasswordInput handleChangePassword={handleStateInputChange} />
+            <PrimaryButton
+              handleClickButton={handleClickButton}
+              text={"登録"}
+              color={theme.palette.primary.main}
+              textColor={theme.palette.textSecondary.main}
+            />
+            <Link to="#">パスワードをお忘れの方</Link>
+            <Or />
+            <p style={{color: "red"}}>\ワンタッチでログイン/</p>
+            <PrimaryButton
+              handleClickButton={handleClickButton}
+              text={"LINEでログイン"}
+              color={theme.palette.line.main}
+              textColor={theme.palette.textSecondary.main}
+            />
+          </CardContent>
+        </BasicCard>
+      </Box>
+    </>
+  );
+};
+
+const cardContentStyle = {
+  gap: 2,
+  display: "flex",
+  flexDirection: "column",
+};
+
+export default LoginForm;
---
+++ frontend/app/src/components/templates/LoginTemplate.tsx
@@ -0,0 +1,19 @@
+import { Box, Container } from "@mui/system";
+import React from "react";
+import LinkCard from "../molecules/LinkCard";
+import LoginForm from "../organisms/LoginForm";
+
+const LoginTemplate: React.FC = () => {
+  return (
+    <>
+      <Container maxWidth="sm">
+        <LoginForm />
+        <Box sx={{ mt: 5, mb: 3 }}>
+          <LinkCard link={"/sign-up"}>アカウントをお持ちでない方</LinkCard>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default LoginTemplate;
